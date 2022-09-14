e451ea60032ec24693ad204e52b6242c46b268df
DScottyS <danielscottsnyder@outlook.com>
2022-09-13 22:55:03 -0400

    Add project files.

---
+++ MP3 Tracker/MP3.cs
@@ -0,0 +1,42 @@
+﻿           /////////////////////////////////////////////////////////////////////////////////////////////////////////
+          //                                                                                                     //
+         //                                                                                                     //
+        // Project: MP3 Tracker                                                                                //
+       // File Name: Project1MP3                                                                              //
+      // Description:                                                                                        //
+     // Course: CSCI 1260 – Introduction to Computer Science II                                             //
+    // Author: Scotty Snyder, snyderds@etsu.edu, Department of Computing, East Tennessee State University  //
+   // Created: Thursday, September 7, 2022                                                                //
+  // Copyright: Scotty Snyder, 2022                                                                      //
+ //                                                                                                     //
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace MP3_Tracker
+{
+    public class MP3
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string ReleaseDate { get; set; }
+        public double PlaybackTime { get; set; }
+        public Genre Genre { get; set; }
+        public decimal DownloadCost { get; set; }
+        public double FileSizeMB { get; set; }
+
+        public string Path = "C:/Users/Scotty/Downloads/Funny_Monkey.jpg";
+
+
+        public override string ToString()
+        {
+            string info = "";
+
+            info += $"MP3 Title: {Title}";
+            info += $"Artist: {Artist}";
+            info += $"Release Date: {ReleaseDate}           Genre:{Genre}";
+            info += $"Download Cost: {DownloadCost}         File Size: {FileSizeMB}";
+            info += $"Playback Time: {PlaybackTime}         Album Photo: {Path}";
+
+            return info;
+        }
+    }
+}
---
+++ MP3 Tracker/MP3Driver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP3_Tracker
+{
+    public class MP3Driver
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to the Funny Monkey MP3 Tracker, please enter your name:");
+            string UserName = Console.ReadLine();
+
+            Console.WriteLine("\n1. Create a new MP3 file" +
+                "\n2. Display an MP3 file" +
+                "\n3. Terminate the program" +
+                "\n\nPlease type the number corresponding to what you would like to do:");
+            int choice = Int32.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    Console.WriteLine($"Thank you for using the program {UserName}, have a great day!");
+                    break;
+            }
+        }
+
+        public static void CreateMP3()
+        {
+
+        }
+    }
+}
